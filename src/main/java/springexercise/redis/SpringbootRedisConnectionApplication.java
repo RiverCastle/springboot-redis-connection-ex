fb69d05d4934d586439045920869c4f67152a558
RiverCastle <rkdtjd12@gmail.com>
2024-04-24 09:39:08 +0900

    first commit

---
+++ src/main/java/springexercise/redis/SpringbootRedisConnectionApplication.java
@@ -0,0 +1,13 @@
+package springexercise.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedisConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRedisConnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springexercise/redis/SpringbootRedisConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package springexercise.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedisConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
